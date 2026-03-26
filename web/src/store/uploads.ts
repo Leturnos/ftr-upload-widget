dcc8814544219d90dc2562dbbdd9f16ae7c8e82b
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2026-03-26 11:31:02 -0300

    refactor(web): integrate Immer for immutable state updates with Maps

--- web/src/store/uploads.ts
+++ web/src/store/uploads.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { enableMapSet } from "immer";
+import { immer } from "zustand/middleware/immer";
 
 export type Upload = {
   name: string;
@@ -10,27 +12,27 @@ type UploadState = {
   addUploads: (files: File[]) => void;
 };
 
-export const useUploads = create<UploadState>((set) => {
-  function addUploads(files: File[]) {
-    for (const file of files) {
-      const uploadId = crypto.randomUUID();
+enableMapSet();
 
-      const upload: Upload = {
-        name: file.name,
-        file,
-      };
+export const useUploads = create<UploadState, [["zustand/immer", never]]>(
+  immer((set) => {
+    function addUploads(files: File[]) {
+      for (const file of files) {
+        const uploadId = crypto.randomUUID();
 
-      set((state) => {
-        const uploads = new Map(state.uploads);
-        uploads.set(uploadId, upload);
+        const upload: Upload = {
+          name: file.name,
+          file,
+        };
 
-        return { uploads };
-      });
+        set((state) => {
+          state.uploads.set(uploadId, upload);
+        });
+      }
     }
-  }
-
-  return {
-    uploads: new Map(),
-    addUploads,
-  };
-});
+    return {
+      uploads: new Map(),
+      addUploads,
+    };
+  })
+);
